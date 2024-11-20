6df74d0b941cc1a8a593e184d8e6f86b2dd21629
Firoj-Ali <meerfiroj81@gmail.com>
2024-11-20 12:58:02 +0530

    add utils file code

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN
 }))
 
+
 app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
